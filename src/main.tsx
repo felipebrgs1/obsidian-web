2c7ae0f2002c8783db0d30d69d4b5092ce52570a
felipeborgaco <felipeborgaco@hotmail.com>
2025-04-11 10:21:58 -0300

    start project

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,35 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import ReactDOM from "react-dom/client";
-import Loader from "./components/loader";
-import { routeTree } from "./routeTree.gen";
-import { queryClient, trpc } from "./utils/trpc";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import ReactDOM from 'react-dom/client';
+import Loader from './components/loader';
+import { routeTree } from './routeTree.gen';
+import { queryClient } from './utils/provider';
 
 const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
-  defaultPendingComponent: () => <Loader />,
-  context: { trpc, queryClient },
-  Wrap: function WrapComponent({ children }) {
-    return (
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    );
-  },
+    routeTree,
+    defaultPreload: 'intent',
+    defaultPendingComponent: () => <Loader />,
+    context: { queryClient },
+    Wrap: function WrapComponent({ children }) {
+        return (
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        );
+    },
 });
 
 // Register things for typesafety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
 }
 
-const rootElement = document.getElementById("app");
-if (!rootElement) throw new Error("Root element not found");
+const rootElement = document.getElementById('app');
+if (!rootElement) throw new Error('Root element not found');
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<RouterProvider router={router} />);
 }
